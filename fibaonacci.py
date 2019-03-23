c63035a95d0da57a950a87df7347bafed58b6e20
Anthony Brown <groundup2360917182914017@gmail.com>
2019-03-23 08:30:35 -0400

    Add files via upload

---
+++ fibaonacci.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+first = 0
+second = 1
+thesum = first + second
+count = int(sys.argv[1])
+thecount = 2 
+
+if count == 0:
+    print("No numbers", end="")
+
+if count == 1:
+    print("0", end="")
+
+if count > 1:
+    print("0 1 ", end="")
+
+while thecount < count:
+    print(thesum, end="")
+
+    if thecount < count - 1:
+        print(" ", end="")
+
+    first = second
+    second = thesum
+    thesum = first + second
+    thecount = thecount + 1
+
+print("")
+
